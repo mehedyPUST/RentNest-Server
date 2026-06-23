7869ff4228577ff6d038d74ee8cc4e17dd67db19
mehedyPUST <mehedy.pust@gmail.com>
2026-06-24 01:16:55 +0600

    properties fetch api added

--- index.js
+++ index.js
@@ -45,6 +45,12 @@ async function run() {
             res.send(result)
         })
 
+
+        app.get('/api/properties', async (req, res) => {
+            const result = await propertiesCollection.find().toArray();
+            res.send(result);
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
