daa58e72a470a9f5d34eea008aaa93fbbdbbc21c
mehedyPUST <mehedy.pust@gmail.com>
2026-06-26 11:37:57 +0600

    better auth url and strict false

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const uri = process.env.MONGODB_URI
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
-        strict: true,
+        strict: false,
         deprecationErrors: true,
     }
 });
